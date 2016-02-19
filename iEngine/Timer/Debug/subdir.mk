f1518f9a650fbb0dc6428fe828a44eff0232e161
mega <truelsy@naver.com>
2016-02-19 15:25:20 +0900

    기존 Timer 삭제

--- iEngine/Timer/Debug/subdir.mk
+++ iEngine/Timer/Debug/subdir.mk
@@ -4,20 +4,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../Timer.cpp 
+../Timer.cpp \
+../TimerHandler.cpp 
 
 OBJS += \
-./Timer.o 
+./Timer.o \
+./TimerHandler.o 
 
 CPP_DEPS += \
-./Timer.d 
+./Timer.d \
+./TimerHandler.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -D__cplusplus=201103L -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -D__cplusplus=201103L -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
